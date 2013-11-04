305c915e44bc05373faa046f19f0a512e1fa3c6d
Keita Ishibashi <fromjpn@gmail.com>
2013-11-05 01:32:12 +0900

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VoxelEditorRails::Application.config.secret_key_base = '4432c5bed9ae7914ce6960e9d87b1b51a56c7008b96029fe576a59ca50fe4e93ba24c008f617262c23a6e886f4306caf066fa6b15d2f91555ab351551ee961eb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VoxelEditorRails::Application.config.session_store :cookie_store, key: '_voxel_editor_rails_session'
